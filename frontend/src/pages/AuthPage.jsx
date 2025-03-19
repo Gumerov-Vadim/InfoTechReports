965904a5a77e605e63aae5751b09271816e47a3f
Gumerov-Vadim <vadim.ugatu@mail.ru>
2025-03-19 07:45:09 +0500

    added initial pages

---
+++ frontend/src/pages/AuthPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AuthPage = () => {
+    return (
+        <div className="auth-page">
+            {/* Здесь будет форма авторизации/регистрации */}
+        </div>
+    );
+};
+
+export default AuthPage; 
---
+++ frontend/src/pages/HomePage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const HomePage = () => {
+    return (
+        <div className="home-page">
+            {/* Здесь будет основной контент приложения */}
+        </div>
+    );
+};
+
+export default HomePage; 
